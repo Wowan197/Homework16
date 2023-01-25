cb1fa57e198a0db43d0566b5d9071e62251d15b6
Wowan197 <123563021+Wowan197@users.noreply.github.com>
2023-01-25 17:12:47 +0300

    Add files via upload

---
+++ Mat_Function.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+int SummaArray(int arr[], int size) {
+    int sum = 0;
+    for (int i = 0; i < size; ++i) {
+        sum += arr[i];
+    }
+    return sum;
+}
+
